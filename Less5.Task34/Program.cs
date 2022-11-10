dd0a26354d1d8e146bb897e0b471f485f9c19d19
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-10 22:14:27 +0700

    add name of task34

--- Less5.Task34/Program.cs
+++ Less5.Task34/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+//int [] array = new int[array.Length];
