5d86fb4a2e9d5bb3b31561a7dc0dceda6707ab23
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-13 16:24:49 +0700

    decided the Task36

--- Less5.Task36/Program.cs
+++ Less5.Task36/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// задать с клавиатуры размер массива
+Console.Write("Введите длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size);
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillandPrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FillandPrintArray(array);
+
+Console.Write("Сумма элементов массива: ");
+void OddSumCount(int[] count)
+{
+    int length = count.Length;
+    int sum = 0;
+    for (int j = 0; j < length; j = j + 2)
+    {
+        sum = sum + array[j];
+    }
+    
+    Console.Write(sum);
+    Console.WriteLine();
+}
+
+OddSumCount(array);
+
+
+
+
+
+
+
+
+
